1a2a40aa1d819574bf2236a1c5d8bf02641c8244
Adrian Nembach <adrian.nembach@knime.com>
2023-07-04 15:32:18 +0200

    Add base class for filestore based vectorstores

--- src/indexes/base.py
+++ src/indexes/base.py
@@ -1,10 +1,19 @@
 # KNIME / own imports
 import knime.extension as knext
+from knime.extension.nodes import (
+    FilestorePortObject,
+    load_port_object,
+    save_port_object,
+    PortType,
+    get_port_type_for_spec_type,
+    get_port_type_for_id
+)
 from models.base import (
     LLMPortObjectSpec,
     LLMPortObject,
     llm_port_type,
     EmbeddingsPortObject,
+    EmbeddingsPortObjectSpec,
 )
 
 # Langchain imports
@@ -14,6 +23,8 @@ from langchain.tools import Tool
 import pandas as pd
 import pickle
 import util
+from typing import Optional, Any
+import os
 
 store_icon = "icons/store.png"
 store_category = knext.category(
@@ -26,6 +37,9 @@ store_category = knext.category(
 
 
 class VectorStorePortObjectSpec(knext.PortObjectSpec):
+    def __init__(self) -> None:
+        super().__init__()
+
     def serialize(self) -> dict:
         return {}
 
@@ -41,10 +55,15 @@ class VectorStorePortObject(knext.PortObject):
         super().__init__(spec)
         self._embeddings_model = embeddings_model
 
+    @property
+    def embeddings_model(self):
+        return self._embeddings_model
+
     def load_store(self, ctx):
         raise NotImplementedError()
 
     def serialize(self) -> bytes:
+        # TODO handle in subclasses and define only interface here
         config = {"embeddings_model": self._embeddings_model}
         return pickle.dumps(config)
 
@@ -52,6 +71,7 @@ class VectorStorePortObject(knext.PortObject):
     def deserialize(
         cls, spec: VectorStorePortObjectSpec, data
     ) -> "VectorStorePortObject":
+        # TODO handle in subclasses
         config = pickle.loads(data)
         return cls(spec, config["embeddings_model"])
 
@@ -59,6 +79,94 @@ class VectorStorePortObject(knext.PortObject):
 vector_store_port_type = knext.port_type(
     "Vectorstore", VectorStorePortObject, VectorStorePortObjectSpec
 )
+
+
+class FilestoreVectorstorePortObjectSpec(VectorStorePortObjectSpec):
+    def __init__(
+        self, embeddings_spec: EmbeddingsPortObjectSpec
+    ) -> None:
+        super().__init__()
+        self._embeddings_port_type = get_port_type_for_spec_type(type(embeddings_spec))
+        self._embeddings_spec = embeddings_spec
+
+    @property
+    def embeddings_port_type(self) -> PortType:
+        return self._embeddings_port_type
+
+    @property
+    def embeddings_spec(self) -> EmbeddingsPortObjectSpec:
+        return self._embeddings_spec
+
+    def serialize(self) -> dict:
+        return {
+            "embeddings_port_type": self.embeddings_port_type.id,
+            "embeddings_spec": self.embeddings_spec.serialize(),
+        }
+
+    @classmethod
+    def deserialize(cls, data: dict):
+        embeddings_port_type: PortType = get_port_type_for_id(data["embeddings_port_type"])
+        embeddings_spec = embeddings_port_type.spec_class.deserialize(
+            data["embeddings_spec"]
+        )
+        return cls(embeddings_spec)
+
+
+class FilestoreVectorstorePortObject(FilestorePortObject, VectorStorePortObject):
+    def __init__(
+        self,
+        spec: FilestoreVectorstorePortObjectSpec,
+        embeddings_port_object: EmbeddingsPortObject,
+        folder_path: Optional[str] = None,
+        vectorstore: Optional[Any] = None,
+    ):
+        # TODO multiple inheritance
+        super().__init__(spec, embeddings_port_object)
+        self._folder_path = folder_path
+        self._vectorstore = vectorstore
+
+    def load_store(self, ctx):
+        if self._vectorstore is None:
+            embeddings = self.embeddings_model.create_model(ctx)
+            self._vectorstore = self.load_vectorstore(
+                embeddings, self._vectorstore_path
+            )
+        return self._vectorstore
+
+    def load_vectorstore(self, embeddings, vectorstore_path):
+        raise NotImplementedError()
+
+    @classmethod
+    def _embeddings_path(cls, folder_path: str) -> str:
+        return os.path.join(folder_path, "embeddings")
+
+    @property
+    def _vectorstore_path(self) -> str:
+        return os.path.join(self._folder_path, "vectorstore")
+
+    def write_to(self, file_path):
+        os.makedirs(file_path)
+        self._folder_path = file_path
+        save_port_object(self.embeddings_model, self._embeddings_path(file_path))
+        self.save_vectorstore(self._vectorstore_path, self._vectorstore)
+
+    def save_vectorstore(self, vectorstore_folder, vectorstore):
+        raise NotImplementedError()
+
+    @classmethod
+    def _read_embeddings(
+        cls, spec: FilestoreVectorstorePortObjectSpec, file_path: str
+    ) -> EmbeddingsPortObject:
+        return load_port_object(
+            spec.embeddings_port_type.object_class,
+            spec.embeddings_spec,
+            cls._embeddings_path(file_path),
+        )
+
+    @classmethod
+    def read_from(cls, spec: FilestoreVectorstorePortObjectSpec, file_path: str):
+        embeddings_obj = cls._read_embeddings(spec, file_path)
+        return cls(spec, embeddings_obj, file_path)
 
 
 class ToolPortObjectSpec(knext.PortObjectSpec):

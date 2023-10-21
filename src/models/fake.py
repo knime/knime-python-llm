a1f67fa7908e84f07f3bf2516e57bf3d8048b7ed
AlexJauchWalser <alexander.jauch-walser@knime.com>
2023-10-21 15:57:59 +0200

    AP-20701: Add Fake Chat Model

--- src/models/fake.py
+++ src/models/fake.py
@@ -4,19 +4,24 @@ from .base import (
     AIPortObjectSpec,
     LLMPortObjectSpec,
     LLMPortObject,
+    ChatModelPortObjectSpec,
+    ChatModelPortObject,
     model_category,
     util,
 )
 
 # Langchain imports
 from langchain.llms.base import LLM
+from langchain.chat_models.base import SimpleChatModel
 from langchain.callbacks.manager import (
     AsyncCallbackManagerForLLMRun,
     CallbackManagerForLLMRun,
 )
+from langchain.schema import BaseMessage
 
 # Other imports
-from typing import Any, List, Optional, Mapping
+from typing import Any, List, Optional, Mapping, Dict
+
 import pickle
 
 fake_icon = "icons/fake.png"
@@ -28,6 +33,8 @@ fake_category = knext.category(
     icon=fake_icon,
 )
 
+# == Settings ==
+
 
 class MissingValueHandlingOptions(knext.EnumParameterOptions):
     DefaultAnswer = (
@@ -38,6 +45,64 @@ class MissingValueHandlingOptions(knext.EnumParameterOptions):
         "Fail",
         "Fail if no matching query is found.",
     )
+
+
+@knext.parameter_group(label="Fake Configuration")
+class FakeGeneralSettings:
+    query_column = knext.ColumnParameter(
+        "Queries",
+        "Column containing the fake queries.",
+        port_index=0,
+        column_filter=util.create_type_filer(knext.string()),
+    )
+
+    response_column = knext.ColumnParameter(
+        "Responses",
+        "Column containing the fake respnses.",
+        port_index=0,
+        column_filter=util.create_type_filer(knext.string()),
+    )
+
+    delay = knext.IntParameter(
+        "Answer delay",
+        "Delays the time the fake LLM will take to respond.",
+        0,
+        min_value=0,
+        is_advanced=True,
+    )
+
+
+@knext.parameter_group(label="Key mismatch")
+class MismatchSettings:
+    handle_key_mismatch = knext.EnumParameter(
+        "Handle key mismatch",
+        """Define whether the LLM should 'answer' with a predefined answer or fail when the prompt can not be found in the provided queries.""",
+        default_value=lambda v: MissingValueHandlingOptions.DefaultAnswer.name,
+        enum=MissingValueHandlingOptions,
+        style=knext.EnumParameter.Style.VALUE_SWITCH,
+    )
+
+    default_response = knext.StringParameter(
+        "Default response",
+        "The response of the fake LLM when no querie matches",
+        "Could not find an answer to the querie.",
+    ).rule(
+        knext.OneOf(
+            handle_key_mismatch, [MissingValueHandlingOptions.DefaultAnswer.name]
+        ),
+        knext.Effect.SHOW,
+    )
+
+
+# == Fake Implementations ==
+
+
+def handle_missing_value(option: str, prompt: str, response: str):
+    if option == MissingValueHandlingOptions.Fail.name:
+        raise knext.InvalidParametersError(
+            f"Could not find matching response for prompt: '{prompt}'. Please make sure, that the prompt exactly matches the provided answers."
+        )
+    return response
 
 
 class FakeDictLLM(LLM):
@@ -53,14 +118,6 @@ class FakeDictLLM(LLM):
         """Return type of llm."""
         return "fake-dict"
 
-    def handle_missing_value(self, prompt: str):
-        if self.option == MissingValueHandlingOptions.Fail.name:
-            raise knext.InvalidParametersError(
-                f"Could not find matching answer for prompt: '{prompt}'. Please make sure, that the prompt exactly matches the provided answers."
-            )
-
-        return self.default_response
-
     def _call(
         self,
         prompt: str,
@@ -68,7 +125,9 @@ class FakeDictLLM(LLM):
         run_manager: CallbackManagerForLLMRun | None = None,
         **kwargs: Any,
     ) -> str:
-        return self.response_dict.get(prompt, self.handle_missing_value(prompt))
+        return self.response_dict.get(
+            prompt, handle_missing_value(self.option, prompt, self.default_response)
+        )
 
     async def _acall(
         self,
@@ -78,7 +137,39 @@ class FakeDictLLM(LLM):
         **kwargs: Any,
     ) -> str:
         """Return next response"""
-        return self.response_dict.get(prompt, self.handle_missing_value())
+        return self.response_dict.get(
+            prompt, handle_missing_value(self.option, prompt, self.default_response)
+        )
+
+    @property
+    def _identifying_params(self) -> Mapping[str, Any]:
+        return {}
+
+
+class FakeChatModel(SimpleChatModel):
+    """Fake ChatModel for testing purposes."""
+
+    response_dict: Dict[str, str]
+    default_response: str
+    option: str
+    sleep: Optional[int] = 0
+
+    @property
+    def _llm_type(self) -> str:
+        return "fake-chat-model"
+
+    def _call(
+        self,
+        messages: List[BaseMessage],
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> str:
+        """First try to lookup in response_dict, else return 'foo' or 'bar'."""
+        prompt = messages[len(messages) - 1].content
+        return self.response_dict.get(
+            prompt, handle_missing_value(self.option, prompt, self.default_response)
+        )
 
     @property
     def _identifying_params(self) -> Mapping[str, Any]:
@@ -167,54 +258,54 @@ fake_llm_port_type = knext.port_type(
 )
 
 
-# == Settings ==
+class FakeChatPortObjectSpec(FakeLLMPortObjectSpec, ChatModelPortObjectSpec):
+    def __init__(self, sleep: float, missing_value_option: str) -> None:
+        super().__init__(sleep, missing_value_option)
 
 
-@knext.parameter_group(label="Fake Configuration")
-class FakeGeneralSettings:
-    query_column = knext.ColumnParameter(
-        "Queries",
-        "Column containing the fake queries.",
-        port_index=0,
-        column_filter=util.create_type_filer(knext.string()),
-    )
+class FakeChatModelPortObject(ChatModelPortObject):
+    def __init__(
+        self,
+        spec: FakeLLMPortObjectSpec,
+        responses: dict[str, str],
+        default_response: str,
+    ) -> None:
+        super().__init__(spec)
+        self._responses = responses
+        self._default_response = default_response
 
-    response_column = knext.ColumnParameter(
-        "Responses",
-        "Column containing the fake respnses.",
-        port_index=0,
-        column_filter=util.create_type_filer(knext.string()),
-    )
+    @property
+    def spec(self) -> FakeChatPortObjectSpec:
+        return super().spec
 
-    delay = knext.IntParameter(
-        "Answer delay",
-        "Delays the time the fake LLM will take to respond.",
-        0,
-        min_value=0,
-        is_advanced=True,
-    )
+    @property
+    def responses(self):
+        return self._responses
+
+    @property
+    def default_response(self):
+        return self._default_response
+
+    def serialize(self):
+        return pickle.dumps((self._responses, self._default_response))
+
+    @classmethod
+    def deserialize(cls, spec, data):
+        (responses, default_response) = pickle.loads(data)
+        return cls(spec, responses, default_response)
+
+    def create_model(self, ctx: knext.ExecutionContext) -> FakeChatModel:
+        return FakeChatModel(
+            response_dict=self.responses,
+            default_response=self.default_response,
+            option=self.spec.missing_value_option,
+            sleep=self.spec.sleep,
+        )
 
 
-@knext.parameter_group(label="Key mismatch")
-class MismatchSettings:
-    handle_key_mismatch = knext.EnumParameter(
-        "Handle key mismatch",
-        """Define whether the LLM should 'answer' with a predefined answer or fail when the prompt can not be found in the provided queries.""",
-        default_value=lambda v: MissingValueHandlingOptions.DefaultAnswer.name,
-        enum=MissingValueHandlingOptions,
-        style=knext.EnumParameter.Style.VALUE_SWITCH,
-    )
-
-    default_response = knext.StringParameter(
-        "Default response",
-        "The response of the fake LLM when no querie matches",
-        "Could not find an answer to the querie.",
-    ).rule(
-        knext.OneOf(
-            handle_key_mismatch, [MissingValueHandlingOptions.DefaultAnswer.name]
-        ),
-        knext.Effect.SHOW,
-    )
+fake_chat_model_port_type = knext.port_type(
+    "Fake Chat Model", FakeChatModelPortObject, FakeChatPortObjectSpec
+)
 
 
 # == Nodes ==
@@ -232,7 +323,7 @@ class MismatchSettings:
 )
 @knext.output_port(
     "Fake LLM",
-    "Configured Fake LLM.",
+    "Configured Fake LLM",
     fake_llm_port_type,
 )
 class FakeLLMConnector:
@@ -277,17 +368,13 @@ class FakeLLMConnector:
                 table_spec, knext.string()
             )
 
-        return FakeLLMPortObjectSpec(
-            self.settings.delay, self.mismatch.handle_key_mismatch
-        )
+        return self.create_spec()
 
     def execute(
         self,
         ctx: knext.ExecutionContext,
         input_table: knext.Table,
     ) -> FakeLLMPortObject:
-        import pandas as pd
-
         df = input_table.to_pandas()
 
         response_dict = dict(
@@ -299,9 +386,98 @@ class FakeLLMConnector:
         )
 
         return FakeLLMPortObject(
-            FakeLLMPortObjectSpec(
-                self.settings.delay, self.mismatch.handle_key_mismatch
-            ),
+            self.create_spec,
             response_dict,
             self.mismatch.default_response,
+        )
+
+    def create_spec(self) -> FakeLLMPortObjectSpec:
+        return FakeLLMPortObjectSpec(
+            self.settings.delay, self.mismatch.handle_key_mismatch
+        )
+
+
+@knext.node(
+    "Fake Chat Model Connector",
+    knext.NodeType.SOURCE,
+    fake_icon,
+    category=fake_category,
+)
+@knext.input_table(
+    "Prompt and answer table",
+    "A table containing a column with expected prompts and one with the matching responses.",
+)
+@knext.output_port(
+    "Fake Chat Model",
+    "Configured Fake Chat Model",
+    fake_chat_model_port_type,
+)
+class FakeChatConnector:
+    """
+    Creates a fake Chat Model.
+
+    This node creates a fake Chat Model implementation for testing purposes without
+    the need to use heavy compute power. Provide a column with expected prompts and their respective
+    answer in a second column. When the 'Fake Chat Model' will be prompted with a matching prompt, it will
+    return the provided answer as an AI message.
+    """
+
+    settings = FakeGeneralSettings()
+    mismatch = MismatchSettings()
+
+    def configure(
+        self,
+        ctx: knext.ConfigurationContext,
+        table_spec: knext.Schema,
+    ) -> FakeChatPortObjectSpec:
+        if self.settings.query_column:
+            util.check_column(
+                table_spec,
+                self.settings.query_column,
+                knext.string(),
+                "String column with prompts",
+            )
+        else:
+            self.settings.query_column = util.pick_default_column(
+                table_spec, knext.string()
+            )
+
+        if self.settings.response_column:
+            util.check_column(
+                table_spec,
+                self.settings.response_column,
+                knext.string(),
+                "String column with responses",
+            )
+        else:
+            self.settings.response_column = util.pick_default_column(
+                table_spec, knext.string()
+            )
+
+        return self.create_spec()
+
+    def execute(
+        self,
+        ctx: knext.ExecutionContext,
+        input_table: knext.Table,
+    ) -> FakeChatModelPortObject:
+        df = input_table.to_pandas()
+
+        response_dict = dict(
+            map(
+                lambda i, j: (i, j),
+                df[self.settings.query_column],
+                df[self.settings.response_column],
+            )
+        )
+
+        return FakeChatModelPortObject(
+            self.create_spec(),
+            response_dict,
+            self.mismatch.default_response,
+        )
+
+    def create_spec(self) -> FakeChatPortObjectSpec:
+        return FakeChatPortObjectSpec(
+            self.settings.delay, self.mismatch.handle_key_mismatch
         )

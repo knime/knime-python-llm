ae939efdc0fa8a7ab267c6132e6689a4629412f0
AlexJauchWalser <alexander.jauch-walser@knime.com>
2024-03-18 08:56:55 +0100

    AP-22020: Add batch size parameter

--- src/models/huggingface/hf_tei.py
+++ src/models/huggingface/hf_tei.py
@@ -3,9 +3,12 @@ import knime.extension as knext
 from ..base import EmbeddingsPortObjectSpec, EmbeddingsPortObject
 from .hf_base import hf_category, hf_icon
 
-
 # Langchain imports
 from langchain_community.embeddings import HuggingFaceHubEmbeddings
+
+# Other imports
+import json
+from typing import List
 
 hf_tei_category = knext.category(
     path=hf_category,
@@ -16,21 +19,83 @@ hf_tei_category = knext.category(
 )
 
 
+class _HuggingFaceEmbeddings(HuggingFaceHubEmbeddings):
+    batch_size: int = 32
+
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        # Overrides HuggingFaceHubEmbeddings 'embed_documents' method for batch_size
+
+        embeddings_vectors: List[List[float]] = []
+
+        # replace newlines, which can negatively affect performance.
+        texts = [text.replace("\n", " ") for text in texts]
+        n_texts = len(texts)
+
+        _model_kwargs = self.model_kwargs or {}
+
+        for index in range(0, n_texts, self.batch_size):
+            batch_end = index + self.batch_size
+
+            text_batch = texts[index:batch_end]
+
+            responses = self.client.post(
+                json={"inputs": text_batch, "parameters": _model_kwargs}, task=self.task
+            )
+
+            vectors = json.loads(responses.decode())
+
+            embeddings_vectors.extend(vectors)
+
+        return embeddings_vectors
+
+    async def aembed_documents(self, texts: List[str]) -> List[List[float]]:
+        # Overrides HuggingFaceHubEmbeddings 'aembed_documents' method for batch_size
+
+        embeddings_vectors: List[List[float]] = []
+
+        # replace newlines, which can negatively affect performance.
+        texts = [text.replace("\n", " ") for text in texts]
+        n_texts = len(texts)
+
+        _model_kwargs = self.model_kwargs or {}
+
+        for index in range(0, n_texts, self.batch_size):
+            batch_end = index + self.batch_size
+
+            text_batch = texts[index:batch_end]
+
+            responses = await self.client.post(
+                json={"inputs": text_batch, "parameters": _model_kwargs}, task=self.task
+            )
+
+            embeddings_vectors.extend(json.loads(responses.decode()))
+
+        return embeddings_vectors
+
+
 class HFTEIEmbeddingsPortObjectSpec(EmbeddingsPortObjectSpec):
-    def __init__(self, inference_server_url: str) -> None:
+    def __init__(self, inference_server_url: str, batch_size: int) -> None:
         super().__init__()
         self._inference_server_url = inference_server_url
+        self._batch_size = batch_size
 
     @property
     def inference_server_url(self):
         return self._inference_server_url
 
+    @property
+    def batch_size(self):
+        return self._batch_size
+
     def serialize(self) -> dict:
-        return {"inference_server_url": self.inference_server_url}
+        return {
+            "inference_server_url": self.inference_server_url,
+            "batch_size": self.batch_size,
+        }
 
     @classmethod
     def deserialize(cls, data: dict):
-        return cls(data["inference_server_url"])
+        return cls(data["inference_server_url"], data["batch_size"])
 
 
 class HFTEIEmbeddingsPortObject(EmbeddingsPortObject):
@@ -42,7 +107,9 @@ class HFTEIEmbeddingsPortObject(EmbeddingsPortObject):
         return super().spec
 
     def create_model(self, ctx) -> HuggingFaceHubEmbeddings:
-        return HuggingFaceHubEmbeddings(model=self.spec.inference_server_url)
+        return _HuggingFaceEmbeddings(
+            model=self.spec.inference_server_url, batch_size=self.spec.batch_size
+        )
 
 
 huggingface_tei_embeddings_port_type = knext.port_type(
@@ -91,7 +158,15 @@ class HFTEIEmbeddingsConnector:
 
     server_url = knext.StringParameter(
         "Text Embeddings Inference Server URL",
-        "The URL where the Text Embeddings Inference server is hosted e.g. `http://localhost:8010/`.",
+        "The URL where the Text Embeddings Inference server is hosted e.g. `http://localhost:8080/`.",
+    )
+
+    batch_size = knext.IntParameter(
+        "Batch size",
+        "How many texts will be send to the embeddings endpoint in one batch.",
+        min_value=1,
+        default_value=32,
+        since_version="5.3.0",
     )
 
     def configure(
@@ -106,4 +181,6 @@ class HFTEIEmbeddingsConnector:
         return HFTEIEmbeddingsPortObject(self.create_spec())
 
     def create_spec(self) -> HFTEIEmbeddingsPortObjectSpec:
-        return HFTEIEmbeddingsPortObjectSpec(self.server_url)
+        batch_size = getattr(self, "batch_size", 32)
+
+        return HFTEIEmbeddingsPortObjectSpec(self.server_url, batch_size)

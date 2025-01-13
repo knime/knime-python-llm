f1cd7f1bdd4f4a568d5a0f653b0ad62b98800189
Yannick Jahn <yannick.jahn@knime.com>
2025-01-13 13:21:02 +0100

    AP-22652: Delete unused LLM Agent Creator

--- src/agents/base.py
+++ src/agents/base.py
@@ -31,13 +31,6 @@ agent_category = knext.category(
 )
 
 
-# agents = {
-#     "CONVERSATIONAL_REACT_DESCRIPTION": AgentType.CONVERSATIONAL_REACT_DESCRIPTION,
-#     "REACT": AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-#     "REACT_DOCUMENTSTORE": AgentType.REACT_DOCSTORE,
-# }
-
-
 @knext.parameter_group(label="Credentials")
 class CredentialsSettings:
     credentials_param = knext.StringParameter(
@@ -51,83 +44,6 @@ class CredentialsSettings:
 class ChatMessageSettings:
     message = knext.MultilineStringParameter(
         "New message", "Specify the new message to be sent to the agent."
-    )
-
-
-# TODO: Currently not used
-@knext.parameter_group(label="LMM Agent Type Selection")
-class LLMAgentTypeSettings:
-    class AgentOptions(knext.EnumParameterOptions):
-        CONVERSATIONAL_REACT_DESCRIPTION = (
-            "Conversational react description",
-            """An agent optimized for conversation. 
-            Other agents are often optimized for using tools to figure 
-            out the best response, which is not ideal in a conversational 
-            setting where you may want the agent to be able to chat with the user as well.
-            
-            For more information see 
-            [LangChain documentation](https://python.langchain.com/docs/modules/agents/agent_types/chat_conversation_agent)
-            """,
-        )
-        REACT = (
-            "ReAct",
-            """Implements the [ReAct paper](https://react-lm.github.io/). 'ReAct prompting is intuitive and flexible to design, 
-            and achieves state-of-the-art few-shot performances across a variety of tasks, from question answering to online shopping'
-
-            For more information see 
-            [LangChain documentation](https://python.langchain.com/docs/modules/agents/agent_types/react)
-            """,
-        )
-        REACT_DOCUMENTSTORE = (
-            "ReAct document store",
-            """Implement the [ReAct logic](https://react-lm.github.io/) for working with document 
-            store specifically.""",
-        )
-
-    agent = knext.EnumParameter(
-        "Agent Type",
-        "Choose between agents that are optimized for different tasks",
-        AgentOptions.CONVERSATIONAL_REACT_DESCRIPTION.name,
-        AgentOptions,
-    )
-
-
-@knext.parameter_group(label="Memory Type Selection")
-class MemoryTypeSettings:
-    class MemoryOptions(knext.EnumParameterOptions):
-        CONVERSATION_BUFFER_MEMORY = (
-            "Conversation Buffer Memory",
-            """Simple yet effective method of storing and recalling past conversations
-            between a human and an AI. It retains the entire history of the conversation,
-            allowing the AI to access and reference previous exchanges with each new
-            message. This ensures that the context of the ongoing conversation is
-            preserved and readily available. However, it's important to note that
-            using this method may lead to a rapid increase in token usage as the
-            conversation progresses.""",
-        )
-        CONVERSATION_BUFFER_WINDOW_MEMORY = (
-            "Conversation Buffer Window Memory",
-            """A more optimized approach to storing and accessing past conversations
-            between a human and an AI. While it retains the input of the entire conversation,
-            it only sends a specified number of recent messages (the "history length") with each
-            new message. This controlled window of conversation history helps manage token
-            usage more efficiently, ensuring that the growth of tokens remains predictable
-            and within a predetermined limit. By limiting the number of messages sent, this
-            method strikes a balance between preserving context and controlling resource consumption.""",
-        )
-
-    memory = knext.EnumParameter(
-        "Memory Type",
-        "Choose between different memory implementations.",
-        MemoryOptions.CONVERSATION_BUFFER_WINDOW_MEMORY.name,
-        MemoryOptions,
-    )
-
-    memory_window_size = knext.IntParameter(
-        "Memory window size",
-        """Specifies the number chat messages the Conversation Buffer Window Memory includes. Only active for Conersation Buffer Window Memory.""",
-        default_value=5,
-        is_advanced=True,
     )
 
 
@@ -193,53 +109,6 @@ class AgentPortObject(FilestorePortObject):
 
 
 agent_port_type = knext.port_type("Agent", AgentPortObject, AgentPortObjectSpec)
-
-
-# @knext.node(
-#     "LLM Agent Creator",
-#     knext.NodeType.SOURCE,
-#     agent_icon,
-#     category=agent_category,
-# )
-# @knext.input_port("LLM", "A large language model.", llm_port_type)
-# @knext.input_port(
-#     "Tool List", "A list of tools for the agent to use.", tool_list_port_type
-# )
-# @knext.output_port("Agent", "A configured agent.", agent_port_type)
-class LLMAgentCreator:
-    """
-    Creates an LLM-based agent equipped with the provided tools.
-
-    Currently, only the **Conversational** agent type is available.
-
-    [Conversational agents](https://python.langchain.com/docs/modules/agents/agent_types/chat_conversation_agent)
-    are optimized for conversation. They expect to be used with a memory component.
-
-    """
-
-    memory_type = MemoryTypeSettings()
-
-    def configure(
-        self,
-        ctx: knext.ConfigurationContext,
-        llm_spec: LLMPortObjectSpec,
-        tools_spec: ToolListPortObjectSpec,
-    ):
-        llm_spec.validate_context(ctx)
-        tools_spec.validate_context(ctx)
-        return AgentPortObjectSpec(self.memory_type.memory, llm_spec, tools_spec)
-
-    def execute(
-        self,
-        ctx: knext.ExecutionContext,
-        llm: LLMPortObject,
-        tools: ToolListPortObject,
-    ):
-        return AgentPortObject(
-            AgentPortObjectSpec(self.memory_type.memory, llm.spec, tools.spec),
-            llm,
-            tools,
-        )
 
 
 @knext.node(
@@ -332,20 +201,6 @@ class AgentPrompter:
                 self.conversation_settings.content_column,
             ]
         ].to_pandas()
-
-        # if agent.spec.memory_type == "CONVERSATION_BUFFER_MEMORY":
-        #     memory = ConversationBufferMemory(
-        #         memory_key="chat_history",
-        #         input_key="input",
-        #         return_messages=True,
-        #     )
-        # else:
-        #     memory = ConversationBufferWindowMemory(
-        #         k=self.history_settings.history_length,
-        #         memory_key="chat_history",
-        #         input_key="input",
-        #         return_messages=True,
-        #     )
 
         # TODO return messages might depend on the type of model (i.e. chat needs it llm doesn't)?
 

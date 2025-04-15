e837f4ab9565b5551d814bf4bcbd9b81370846ea
Adrian Nembach <adrian.nembach@knime.com>
2025-04-15 12:44:23 +0200

    AP-24077: Prevent unused import warning

--- src/models/ibm/__init__.py
+++ src/models/ibm/__init__.py
@@ -1,3 +1,9 @@
 from ._auth import IBMwatsonxAuthenticator
 from ._chat import IBMwatsonxChatModelConnector
 from ._embedding import IBMwatsonxEmbeddingModelConnector
+
+__all__ = [
+    "IBMwatsonxAuthenticator",
+    "IBMwatsonxChatModelConnector",
+    "IBMwatsonxEmbeddingModelConnector",
+]

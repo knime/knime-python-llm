b445de8969878dbedb715cf486157145a551b5f8
Adrian Nembach <adrian.nembach@knime.com>
2025-05-30 14:52:26 +0200

    AP-24293: Fix content type switch
    AP-24293: Add toString to TextContentPart

--- org.knime.ai.core/src/main/java/org/knime/ai/core/data/message/TextContentPart.java
+++ org.knime.ai.core/src/main/java/org/knime/ai/core/data/message/TextContentPart.java
@@ -55,13 +55,22 @@ import org.knime.ai.core.data.message.MessageValue.MessageContentPart;
  *
  * @author Adrian Nembach, KNIME GmbH, Konstanz, Germany
  */
-public class TextContentPart implements MessageContentPart {
+public final class TextContentPart implements MessageContentPart {
+
     private final String m_content;
 
+    /**
+     * Creates a new text content part with the given content.
+     *
+     * @param content the text content of this part
+     */
     public TextContentPart(final String content) {
         m_content = content;
     }
 
+    /**
+     * @return the content of this text part
+     */
     public String getContent() {
         return m_content;
     }
@@ -74,6 +83,11 @@ public class TextContentPart implements MessageContentPart {
     @Override
     public byte[] getData() {
         return m_content.getBytes();
+    }
+
+    @Override
+    public String toString() {
+        return m_content;
     }
 
 }

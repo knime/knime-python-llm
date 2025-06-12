bb9fa9f0622ce2aee0da2071fc0135fcb08a91ed
Ivan Prigarin <ivan.prigarin@knime.com>
2025-06-12 19:08:37 +0200

    AP-24494: Introduce LLM Chat Prompter
    AP-24494 (LLM Chat Prompter requires an empty table to be provided when starting a new conversation)

--- src/models/prompters/__init__.py
+++ src/models/prompters/__init__.py
@@ -43,35 +43,4 @@
 # ------------------------------------------------------------------------
 
 
-import indexes.base
-import indexes.faiss
-import indexes.chroma
-
-import models.base
-import models.prompters
-import models.huggingface
-import models.fake
-import models.openai
-import models.knime
-import models.databricks
-import models.deepseek
-import models.ibm
-import models.google
-import models.anthropic_api
-
-try:
-    import models.gpt4all
-except Exception as e:
-    print(f"Could not import gpt4all: {e}")
-
-import models.azure
-
-import agents.base
-import agents.openai
-
-import tools.base
-import tools.vectorstore
-
-import util_nodes.base
-
-import eval.giskard
+from ._chat import ChatPrompter
